823683e5c4841aa3c862557a5438df55c9dd391c
Joshua-Ashton01 <joshua.t.ashton@gmail.com>
2022-11-08 16:30:25 -0700

    guiVersion of project, rewriting code for maximum clarity

---
+++ 1410_DND/src2/DndCharacterBuilder.java
@@ -0,0 +1,11 @@
+public class DndCharacterBuilder {
+
+    public static void main(String[] args) {
+
+        PanelHolder guiPanels = new PanelHolder();
+
+        UImanager gui = new UImanager(guiPanels);
+
+    }
+
+}
---
+++ 1410_DND/src2/PanelHolder.java
@@ -0,0 +1,129 @@
+public static class PanelHolder {
+
+    private JPanel mainMenuPanel;
+    private JPanel racePanel;
+    private JPanel classPanel;
+    private JPanel abilityScorePanel;
+    private JPanel descriptionPanel;
+    private JPanel equipmentPanel;
+    private JPanel finalizePanel;
+    private JPanel loadMenuPanel;
+
+    public PanelHolder() {
+
+        this.mainMenuPanel = createMainMenuPanel();
+        this.racePanel = createRaceSelectPanel();
+        this.classPanel = createClassSelectPanel();
+        this.abilityScorePanel = createAbilityScoreSelectPanel();
+        this.descriptionPanel = createDescriptionPanel();
+        this.equipmentPanel = createEquipmentSelectPanel();
+        this.finalizePanel = createFinalizePanel();
+        this.loadMenuPanel = createLoadMenu();
+
+    }
+
+    public JPanel getMainMenuPanel() {
+
+        return mainMenuPanel;
+
+    }
+
+    public JPanel getRacePanel() {
+
+        return racePanel;
+
+    }
+
+    public JPanel getClassPanel() {
+
+        return classPanel;
+
+    }
+
+    public JPanel getAbilityScorePanel() {
+
+        return abilityScorePanel;
+
+    }
+
+    public JPanel getDescriptionPanel() {
+
+        return descriptionPanel;
+
+    }
+
+    public JPanel getEquipmentPanel() {
+
+        return equipmentPanel;
+
+    }
+
+    public JPanel getFinalizePanel() {
+
+        return finalizePanel;
+
+    }
+
+    public JPanel getLoadMenuPanel() {
+
+        return loadMenuPanel;
+
+    }
+
+    // TODO: Need to implement.
+    private JPanel createMainMenuPanel() {
+
+
+
+    }
+
+    // TODO: Need to implement.
+    private JPanel createRaceSelectPanel() {
+
+
+
+    }
+
+    // TODO: Need to implement.
+    private JPanel createClassSelectPanel() {
+
+
+
+    }
+
+    // TODO: Need to implement.
+    private JPanel createAbilityScoreSelectPanel() {
+
+
+
+    }
+
+    // TODO: Need to implement.
+    private JPanel createDescriptionPanel() {
+
+
+
+    }
+
+    // TODO: Need to implement.
+    private JPanel createEquipmentSelectPanel() {
+
+
+
+    }
+
+    // TODO: Need to implement.
+    private JPanel createFinalizePanel() {
+
+
+
+    }
+
+    // TODO: Need to implement.
+    private JPanel createLoadMenu() {
+
+
+
+    }
+
+}
