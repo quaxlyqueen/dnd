801ebe1b151d17f67636b63455984942a44322b0
Joshua-Ashton01 <joshua.t.ashton@gmail.com>
2022-11-15 11:58:40 -0700

    creating imagePanel class

---
+++ dndBuilder/src/GUI/Panels/img/ImagePanel.java
@@ -0,0 +1,23 @@
+package GUI.Panels.img;
+
+import javax.swing.*;
+import java.io.File;
+
+public class ImagePanel extends JPanel {
+
+    private JLabel[] dragonbornPortraits;
+
+    public ImagePanel() {
+
+        super();
+
+
+    }
+
+    private void createPortraitsArray() {
+
+
+
+    }
+
+}
